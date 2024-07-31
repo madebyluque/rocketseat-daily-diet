fe06a0d80c4e453bc684c5bc057c1126d8a7707b
Lucas Silva <lukelimasilva@gmail.com>
2024-07-31 08:24:38 -0300

    add create-meals migration

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    meals: {
+      id: string
+      name: string
+      description: string
+      datetime: string
+      within_diet: boolean
+    }
+  }
+}
