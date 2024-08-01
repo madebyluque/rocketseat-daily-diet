278505c51cf1ed3604045ed926cb7b7a946593d3
Lucas Silva <lukelimasilva@gmail.com>
2024-08-01 19:32:11 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,18 @@ import { fastifyJwt } from '@fastify/jwt'
 import { env } from './env'
 import { usersRoutes } from './users/routes/users.routes'
 import { mealsRoutes } from './meals/routes/meals.routes'
+import cors from '@fastify/cors'
 
 export const app = fastify()
+
+app.register(cors, {
+  origin: [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+})
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
